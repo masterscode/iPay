998b32dc0da7ad39b1b10bccfc055a7f45364c09
Emmanuel Ogbinaka <ogbinakaemmanuel@gmail.com>
2023-12-18 11:09:42 +0100

    Add kafka

--- src/main/java/com/Index/service/transaction/TransactionService.java
+++ src/main/java/com/Index/service/transaction/TransactionService.java
@@ -2,18 +2,10 @@ package com.Index.service.transaction;
 
 import com.Index.entities.Transaction;
 import com.Index.payloads.ApiResponse;
-import com.Index.payloads.BankTransferResponse;
-
-import java.util.Optional;
 
 public interface TransactionService {
-    BankTransferResponse getTransactionStatus(String transactionReference);
 
     Transaction saveTransaction(Transaction t);
-
-    Optional<Transaction> getTransaction(String ref);
-
-    boolean transactionExists(String ref);
 
     ApiResponse findTransaction(String ref);
 }
--- src/main/java/com/Index/service/transaction/TransactionServiceImpl.java
+++ src/main/java/com/Index/service/transaction/TransactionServiceImpl.java
@@ -3,7 +3,6 @@ package com.Index.service.transaction;
 
 import com.Index.entities.Transaction;
 import com.Index.payloads.ApiResponse;
-import com.Index.payloads.BankTransferResponse;
 import com.Index.repos.TransactionRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -17,20 +16,10 @@ import java.util.Optional;
 public class TransactionServiceImpl implements TransactionService {
     private final TransactionRepository transactionRepository;
 
-    @Override
-    public BankTransferResponse getTransactionStatus(final String transactionReference) {
-        Transaction transaction = transactionRepository.findByTransactionReference(transactionReference).orElseThrow();
-        return new BankTransferResponse(transaction);
-    }
 
     @Override
     public Transaction saveTransaction(Transaction t){
         return transactionRepository.save(t);
-    }
-
-    @Override
-    public Optional<Transaction> getTransaction(final String ref) {
-        return transactionRepository.findByTransactionReference(ref);
     }
 
     @Override
